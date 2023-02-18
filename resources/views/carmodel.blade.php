7fffccdd94c66ccc758c7ed5ad1072bea3359f7e
sharynelalaa <118233524+sharynelalaa@users.noreply.github.com>
2023-02-18 16:15:09 +0800

    added icons

--- resources/views/carmodel.blade.php
+++ resources/views/carmodel.blade.php
@@ -12,10 +12,10 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
   <link href="{{ asset('css/carmodel.css') }}" rel="stylesheet" />
-  <title>G'Five Rentals</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>G'Five Rentals</title>
 </head>
 
 <body>
